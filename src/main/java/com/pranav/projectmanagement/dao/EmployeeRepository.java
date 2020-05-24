478687219addcd4d5e2480024e25f07c83ee1cf7
Pranav Gupta <pranavgupta9797@gmail.com>
2020-05-24 14:16:24 -0500

    Added Employee Table to Home Page

--- src/main/java/com/pranav/projectmanagement/dao/EmployeeRepository.java
+++ src/main/java/com/pranav/projectmanagement/dao/EmployeeRepository.java
@@ -3,6 +3,10 @@ package com.pranav.projectmanagement.dao;
 import com.pranav.projectmanagement.entities.Employee;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface EmployeeRepository extends CrudRepository<Employee, Long> {
 
+    @Override
+    List<Employee> findAll();
 }
